1afb297df1d2019972773e05f6d10523efdb6042
Sri Aspari <sriaspari@gmail.com>
2021-03-11 17:17:58 +0700

    feat: 🎉 initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="snowreact">
+      <div className="container">
+        <h1 className="title">Welcome to Snowreact</h1>
+        <p className="subtitle">Snowpack + React.js boilerplate</p>
+        <a href="https://www.snowpack.dev" className="button">
+          Snowpack
+        </a>
+        <a href="https://reactjs.org" className="button">
+          React.js
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
